d27e94d85a76abc7840a6b0a03cbbda71b86106d
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-05-31 10:58:49 -0600

    Setup chat completions from OpenAI inside orthographyCheckUseCase

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+
+import OpenAI from 'openai';
+
 import { orthographyCheckUseCase } from './use-cases';
 import { OrthographyDto } from './dtos';
 
 // Services are a centralized place to maintain information
 @Injectable()
 export class GptService {
+  private openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
+
   // It'll just call the use-cases
 
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyCheckUseCase({
+    return await orthographyCheckUseCase(this.openai, {
       prompt: orthographyDto.prompt,
     });
   }
--- src/gpt/use-cases/orthography.use-case.ts
+++ src/gpt/use-cases/orthography.use-case.ts
@@ -1,13 +1,30 @@
-// eslint-disable-next-line @typescript-eslint/require-await
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
 }
 
-export const orthographyCheckUseCase = async (options: Options) => {
+export const orthographyCheckUseCase = async (
+  openai: OpenAI,
+  options: Options,
+) => {
   const { prompt } = options;
 
-  return {
-    prompt: prompt,
-    apikey: process.env.OPENAI_API_KEY,
-  };
+  const completion = await openai.chat.completions.create({
+    messages: [
+      { role: 'system', content: 'Eres un asistente muy útil.' },
+      { role: 'user', content: prompt },
+    ],
+    model: 'gpt-4o',
+  });
+
+  console.log(completion);
+  console.log(prompt);
+
+  return completion.choices[0].message.content;
+
+  // return {
+  //   prompt: prompt,
+  //   apikey: process.env.OPENAI_API_KEY,
+  // };
 };
