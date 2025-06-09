4430b6690de19f643065abed74e77480710da5f0
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-08 18:46:48 -0600

    Added functionality to receive the prompt as audio from openai and save it as an mp3 file

--- src/gpt/use-cases/text-to-audio.use-case.ts
+++ src/gpt/use-cases/text-to-audio.use-case.ts
@@ -1,5 +1,6 @@
 import OpenAI from 'openai';
 import * as path from 'path';
+import * as fs from 'fs';
 
 interface Options {
   prompt: string;
@@ -10,17 +11,40 @@ export const textToAudioUseCase = async (
   openai: OpenAI,
   { prompt, voice }: Options,
 ) => {
-  const voices = ['nova', 'alloy'];
+  const voices = ['nova', 'alloy', 'echo', 'fable', 'onyx', 'shimmer'];
+  // const voices = {
+  //   nova: 'nova',
+  //   alloy: 'alloy',
+  //   echo: 'echo',
+  //   fable: 'fable',
+  //   onyx: 'onyx',
+  //   shimmer: 'shimmer',
+  // };
 
-  // const selectedVoice = voices[i] ?? 'nova';
+  // const selectedVoice = voices[voice] ?? 'nova';
   const selectedVoice = voices.includes(voice ?? '')
     ? (voice as string)
     : 'nova';
 
-  const folderPath = path.resolve( __dirname, '../../../generated/audios/')
+  const folderPath = path.resolve(__dirname, '../../../generated/audios/');
+  const speechFile = path.resolve(`${folderPath}/${new Date().getTime()}.mp3`);
 
-  return {
-    prompt: prompt,
-    selectedVoice: selectedVoice,
-  };
+  const mp3 = await openai.audio.speech.create({
+    model: 'tts-1',
+    voice: selectedVoice,
+    input: prompt,
+    response_format: 'mp3',
+  });
+
+  // console.log(mp3);
+  const buffer = Buffer.from(await mp3.arrayBuffer());
+  fs.writeFileSync(speechFile, buffer);
+
+  fs.mkdirSync(folderPath, { recursive: true });
+
+  return speechFile;
+  // return {
+  //   prompt: prompt,
+  //   selectedVoice: selectedVoice,
+  // };
 };
