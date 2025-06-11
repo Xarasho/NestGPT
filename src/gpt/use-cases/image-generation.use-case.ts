5bd4a162703c933de0b64e045d2b44215a727664
Alejandro Rojas <alex.olmedo.rojas@gmail.com>
2025-06-11 02:04:42 -0600

    Can now receive the url of the generated image

--- src/gpt/use-cases/image-generation.use-case.ts
+++ src/gpt/use-cases/image-generation.use-case.ts
@@ -1,5 +1,8 @@
+import * as fs from 'fs';
+// import * as path from 'path';
+
 import OpenAI from 'openai';
-import { downloadImageAsPng } from 'src/helpers';
+import { downloadBase64ImageAsPng, downloadImageAsPng } from 'src/helpers';
 
 interface Options {
   prompt: string;
@@ -11,17 +14,52 @@ export const imageGenerationUseCase = async (
   openai: OpenAI,
   options: Options,
 ) => {
-  // const { prompt, originalImage, maskImage } = options;
-  const { prompt } = options;
+  const { prompt, originalImage, maskImage } = options;
 
   // TODO: VERIFY ORIGINAL IMAGE
+  if (!originalImage || !maskImage) {
+    const response = await openai.images.generate({
+      prompt: prompt,
+      model: 'dall-e-3',
+      n: 1,
+      size: '1024x1024',
+      quality: 'standard',
+      response_format: 'url',
+    });
 
-  const response = await openai.images.generate({
-    prompt: prompt,
+    if (
+      !response.data ||
+      !Array.isArray(response.data) ||
+      !response.data[0] ||
+      !response.data[0].url
+    ) {
+      throw new Error('Image generation failed: No data returned from OpenAI');
+    }
+
+    // Todo: Save file in file system
+    const fileName = await downloadImageAsPng(response.data[0].url);
+    const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
+    // console.log(response);
+
+    return {
+      url: url,
+      openAIUrl: response.data[0].url,
+      revised_prompt: response.data[0].revised_prompt,
+    };
+  }
+
+  // originalImage= http://172.26.16.116:3000/gpt/image-generation/1749597767837.png
+  // maskImage=Base64;fakdsjhf9784t7239p2qy5p94h5p4959q8435ugvn845845nq84y5gvn84q25
+  const pngImagePath = await downloadImageAsPng(originalImage);
+  const maskPath = await downloadBase64ImageAsPng(maskImage);
+
+  const response = await openai.images.edit({
     model: 'dall-e-3',
+    prompt: prompt,
+    image: fs.createReadStream(pngImagePath),
+    mask: fs.createReadStream(maskPath),
     n: 1,
     size: '1024x1024',
-    quality: 'standard',
     response_format: 'url',
   });
 
@@ -31,13 +69,15 @@ export const imageGenerationUseCase = async (
     !response.data[0] ||
     !response.data[0].url
   ) {
-    throw new Error('Image generation failed: No data returned from OpenAI');
+    throw new Error('Image editing failed: No data returned from OpenAI');
   }
 
-  // Todo: Save file in file system
-  const url = await downloadImageAsPng(response.data[0].url);
+  // const localImagePath = await downloadImageAsPng(response.data[0].url);
+  // const fileName = path.basename(localImagePath);
+  // const publicUrl = `localhost:3000/${fileName}`;
 
-  // console.log(response);
+  const fileName = await downloadImageAsPng(response.data[0].url);
+  const url = `${process.env.SERVER_URL}/gpt/image-generation/${fileName}`;
 
   return {
     url: url,
